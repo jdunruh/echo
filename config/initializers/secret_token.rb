d1698c279dfa0e627145feb15ebee4aed94c0001
John Unruh <j_unruh@hotmail.com>
2013-08-14 11:29:03 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EChO::Application.config.secret_token = 'd475b73efac86af6ade1b14753df14cc2977c3c4d5f4380c73b8534757d7dae839c89a0aed2e399832fc3fb17d2cd4f2ab4cf458d514f045fe8d4a85225d695b'
